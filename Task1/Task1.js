9d5db61f4059ed3e3613d2b00f2ef298c2d141cb
soumyatech-byte <soumyad013@gmail.com>
2025-09-19 17:28:19 +0530

    Add files via upload

---
+++ Task1/Task1.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("alertBtn");
+  btn.addEventListener("click", () => {
+    alert("Hello! You clicked the button 🎉");
+  });
+});
